2d019f54a45ab3fad5d37b0fca07a995d20b1271
Yunpeng Niu <neilniuyunpeng@gmail.com>
2021-04-11 20:17:38 +0800

    Init webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'secure-collect.js',
+    library: {
+    	name: 'secureCollect',
+    	type: 'umd'
+    }
+  },
+};
