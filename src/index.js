a1552eb10a50b5d22bc581ebc46b83afcff6e662
Yunpeng Niu <neilniuyunpeng@gmail.com>
2021-04-11 23:40:41 +0800

    Fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 export function init() {
-	alert('Haha');
+	// Imports all declared keys.
+	const keys = importInputKeys();
+	console.log(keys);
+}
+
+function importInputKeys() {
+	const keys = new Map();
+
+	// Iterates over every meta tag with the desired name.
+	const tags = document.getElementsByTagName("meta");
+	for (let i = 0; i < tags.length; i++) {
+		const meta = tags[i];
+
+		// Skips if the meta name does not match.
+		if (meta.getAttribute("name") !== "input-key") {
+			return;
+		}
+
+		// Gets the content.
+		const content = meta.getAttribute("content");
+
+		// Splits to process each key.
+		content.split(",").forEach(element => {
+			const trimmed = element.trim();
+
+			// Finds out the key name & value.
+			if (trimmed.split("=").length !== 2) {
+				console.log("The given key '" + trimmer + "' is in invalid format");
+				return;
+			}
+			const key = trimmed.split("=")[0].trim();
+			const val = trimmed.split("=")[1].trim();
+
+			// Inserts the found key.
+			keys.set(key, val);
+		});
+	}
+
+	// Returns all the input keys declared.
+	return keys;
 }
